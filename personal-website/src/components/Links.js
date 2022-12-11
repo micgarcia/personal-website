66e9c60db4e55bab595aea06da0f5dc3e454bad4
micgarcia <micgarcia9185@gmail.com>
2022-12-11 15:41:59 -0800

    Added links, increased footer size

--- personal-website/src/components/Links.js
+++ personal-website/src/components/Links.js
@@ -2,15 +2,11 @@
 function Links() {
   return (
     <div className="linksCont">
-      <div className="links">
-        <h4>Github</h4>
-        <h4>Linkedin</h4>
-      </div>
-      <div className="contact">
-        <h4>garciam9185@gmail.com</h4>
-        <h4>(916) 257-9629</h4>
-      </div>
+      <a className="github" href="https://github.com/micgarcia">Github</a>
+      <a className="linkedin" href="https://www.linkedin.com/in/michael-garcia-developer/">Linkedin</a>
 
+      <h4 className="emailLink">garciam9185@gmail.com</h4>
+      <h4 className="phoneNum">(916) 257-9629</h4>
     </div>
   )
 }
