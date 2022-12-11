01cfeb3aa0010ff289a4905ec051f08006e4ac35
micgarcia <micgarcia9185@gmail.com>
2022-12-11 15:54:30 -0800

    cleaned up footer and links, added icons to footer

--- personal-website/src/components/Links.js
+++ personal-website/src/components/Links.js
@@ -2,11 +2,24 @@
 function Links() {
   return (
     <div className="linksCont">
-      <a className="github" href="https://github.com/micgarcia">Github</a>
-      <a className="linkedin" href="https://www.linkedin.com/in/michael-garcia-developer/">Linkedin</a>
 
-      <h4 className="emailLink">garciam9185@gmail.com</h4>
-      <h4 className="phoneNum">(916) 257-9629</h4>
+      <div class="gitCont">
+        <span class="material-symbols-outlined">person</span>
+        <a className="github" href="https://github.com/micgarcia">Github</a>
+      </div>
+      <div class="linkedinCont">
+        <span class="material-symbols-outlined">group</span>
+        <a className="linkedin" href="https://www.linkedin.com/in/michael-garcia-developer/">Linkedin</a>
+      </div>
+
+      <div className="emailCont">
+        <span class="material-symbols-outlined">mail</span>
+        <h4 className="emailLink">garciam9185@gmail.com</h4>
+      </div>
+      <div class="phoneCont">
+        <span class="material-symbols-outlined">phone</span>
+        <h4 className="phoneNum">(916) 257-9629</h4>
+      </div>
     </div>
   )
 }
