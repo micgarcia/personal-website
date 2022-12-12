d88c4c2d11f6121d811ba93efb999cc57f72aa30
micgarcia <micgarcia9185@gmail.com>
2022-12-12 14:27:03 -0800

    Added projects section, picture, description, spacing and styling. Added navbar component to top of page, increased title container size

--- personal-website/src/components/Homepage.js
+++ personal-website/src/components/Homepage.js
@@ -3,11 +3,13 @@ import Bio from "./Bio.js";
 import Skills from "./Skills.js";
 import Projects from "./Projects.js";
 import Links from "./Links.js";
+import Navbar from "./Navbar.js";
 import 'material-symbols';
 
 function Homepage() {
   return (
     <div className="homePage">
+      <Navbar />
       <Title />
       <Bio />
       <Skills />
--- personal-website/src/components/Projects.js
+++ personal-website/src/components/Projects.js
@@ -4,11 +4,18 @@ function Projects() {
       <div className="projectOne">
         <h1>Movie Tracker</h1>
         <div className="movieProjPic"></div>
-        <p>Project Description</p>
+        <p>Web App to search for movies by genre or popularity and save the ones
+          that you want to watch later. Developed using <b>React.js</b> and <b>Firebase</b> for
+          backend services.</p>
       </div>
       <div className="projectTwo">
-        <h1>Project Two</h1>
-        <p>Project Description</p>
+        <h1>Inventory App</h1>
+        <div className="invProjPic"></div>
+        <p>Mock Inventory App developed using <b>Node.js/Express</b>, <b>MongoDB</b> for the database,
+          and <b>PUG</b> view templates for the server side rendering. This project demonstrates
+          my proficiency with implementing <b>REST API</b> principles and <b>MVC</b> architectures within
+          a <b>SSR application</b>.
+          </p>
       </div>
     </div>
   )
